f9792e5e3d357a0d1fb8459d4cfc6f24dbc05c79
Hermanda Mak <hermandamak@gmail.com>
2023-08-05 17:09:57 -0700

    add currency api call and display

--- client/src/components/Currency.tsx
+++ client/src/components/Currency.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type CurrencyDataType = {
+  base?: string
+  date?: string
+  motd?:{
+    [key: string]: number
+  }
+  rates?:{
+    [key: string]: number
+  }
+  success: boolean
+}
+
+
 const Currency = () => {
+
+  const [currencyData, setCurrencyData] = useState<CurrencyDataType>({success: false})
+
+  useEffect(() => {
+    axios.get(`https://api.exchangerate.host/latest?base=USD`)
+      .then(res => 
+        setCurrencyData(res.data)
+      )
+      .catch(err => console.error(err))
+  },[]);
+
   return (
-    <div>Currency</div>
+    <div>
+      <h2>Currency</h2>
+      <div>From USD to JPY : {currencyData.rates?.["JPY"]}</div>
+      <div>as recorded on {currencyData.date}</div>
+    </div>
   )
 }
 export default Currency
