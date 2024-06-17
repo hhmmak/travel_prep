1e79de63755d9b06aa17647d9da42103d3309e60
Hermanda Mak <hermandamak@gmail.com>
2024-06-17 13:14:18 -0700

    fix minor bug

--- client/src/components/Weather.tsx
+++ client/src/components/Weather.tsx
@@ -41,21 +41,21 @@ const Weather = () => {
   // lat, lon initiated to Tokyo, Japan
   const [lat, setLat] = useState<number>(35.6762)
   const [lon, setLon] = useState<number>(139.6503)
-  
-  const days : number[] = [0, 1, 2, 3, 4, 5, 6];
 
-  
+  const days: number[] = [0, 1, 2, 3, 4, 5, 6];
+
+
   useEffect(() => {
 
-    try{
+    try {
       axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timeformat=unixtime&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&current_weather=true&timezone=GMT`)
         .then(res => setWeatherData(res.data))
         .catch(err => console.log(err))
-    } catch(err) {
+    } catch (err) {
       console.log(err)
     }
 
-  },[lat, lon]);
+  }, [lat, lon]);
 
   return (
     <div>
@@ -68,14 +68,18 @@ const Weather = () => {
         <h3>5 Day Weather</h3>
         <div>
           <hr />
-          { days.map((day: number) => 
+          {days.map((day: number) =>
             <div key={`day-${day}`}>
-              <div>Date : {weatherData && dateFormat(weatherData.daily?.time[day] ?? "")}</div>
-              <div>Weather Code : {weatherData?.daily?.weathercode[day]} </div>
-              <div>Max Temp : {weatherData?.daily?.temperature_2m_max[day]} </div>
-              <div>Min Temp : {weatherData?.daily?.temperature_2m_min[day]} </div>
-              <div>Apparent Max Temp : {weatherData?.daily?.apparent_temperature_max[day]} </div>
-              <div>Apparent Min Temp : {weatherData?.daily?.apparent_temperature_min[day]} </div>
+              {weatherData && weatherData.daily &&
+                <>
+                  <div>Date : {dateFormat(weatherData.daily.time[day] ?? "")}</div>
+                  <div>Weather Code : {weatherData.daily.weathercode[day]} </div>
+                  <div>Max Temp : {weatherData.daily.temperature_2m_max[day]} </div>
+                  <div>Min Temp : {weatherData.daily.temperature_2m_min[day]} </div>
+                  <div>Apparent Max Temp : {weatherData.daily.apparent_temperature_max[day]} </div>
+                  <div>Apparent Min Temp : {weatherData.daily.apparent_temperature_min[day]} </div>
+                </>
+              }
             </div>
           )}
         </div>
