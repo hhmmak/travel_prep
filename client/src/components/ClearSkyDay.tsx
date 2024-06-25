740292bad121b08ad71656c51894d5362dc9b4b1
Hermanda Mak <hermandamak@gmail.com>
2024-06-25 10:55:44 -0700

    set up svg for weather

---
+++ client/src/components/ClearSkyDay.tsx
@@ -0,0 +1,8 @@
+import {ReactComponent as Icon} from '../assets/clear-sky-day.svg'
+
+const ClearSkyDay = ({width=50, height=50}: {width?: number, height?: number}) => {
+  return (
+    <Icon width={`${width}px`} height={`${height}px`} />
+  )
+}
+export default ClearSkyDay
