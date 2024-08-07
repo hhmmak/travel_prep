3564c06608f6e89866db32c80c01d8ce165bdfc1
Hermanda Mak <hermandamak@gmail.com>
2024-08-07 13:56:27 -0700

    add settings

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,13 +1,15 @@
 import Weather from "./Weather"
 import Currency from "./Currency"
 import Location from "./Location"
+import Setting from "./Setting"
 
 import SessionContainer from './SessionContainer'
 
 const Home = () => {
   return (
     <div>
-      <h1>Travel Prep</h1>
+      <h1 className="text-2xl text-center m-5">Travel Prep</h1>
+      <Setting />
       <Location />
       <SessionContainer>
         <Weather />
--- client/src/components/Setting.tsx
+++ client/src/components/Setting.tsx
@@ -1,6 +1,6 @@
 const Setting = () => {
   return (
-    <div>
+    <div className="m-4 p-4 border-l-4 border-b-4 rounded-lg">
       <label htmlFor="city-dropdown">Destination : </label>
       <select id="city-dropdown" onChange={() => { }} className="border p-1 w-12">
         <option key={"celcius"} value={"celcius"}>
