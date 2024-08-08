00463b5478827e919b617bb1d733a8293d93d973
Hermanda Mak <hermandamak@gmail.com>
2024-08-08 12:03:51 -0700

    set up location flex to currency fetching

--- client/src/components/Currency.tsx
+++ client/src/components/Currency.tsx
@@ -1,33 +1,40 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import currencyFormat from "../util/currencyFormat";
+import { useLocation } from "../hooks/LocationService";
+import currencyCode from "../util/currencyCode";
 
-type CurrencyDataType<FromCurrency extends string> = {
+type CurrencyDataType = {
   date : string 
 } & {
-  [key in FromCurrency] : {[toCurrency: string]: number}
+  [key: string] : {[toCurrency: string]: number}
 }
 
 
 const Currency = () => {
+  const [fromCurrency, setFromCurrency] = useState<string>("usd")
+  const [toCurrency, setToCurrency] = useState<string>("hkd")
+  const [currencyData, setCurrencyData] = useState<CurrencyDataType | null>(null)
+  const {origin, destinationCity, destinationCountry} = useLocation()
 
-  const [currencyData, setCurrencyData] = useState<CurrencyDataType<"usd"> | null>(null)
-
+  // Currency API credit to: https://github.com/fawazahmed0/exchange-api
   useEffect(() => {
-    axios.get(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${"usd"}.json`)
+    setFromCurrency(currencyCode[origin]?.toLowerCase())
+    setToCurrency(currencyCode[destinationCity]?.toLowerCase() || currencyCode[destinationCountry]?.toLowerCase())
+    axios.get(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}.json`)
       .then(res => 
         setCurrencyData(res.data)
         // console.log(res.data)
       )
       .catch(err => console.error(err))
-  },[]);
+  },[destinationCity, destinationCountry, fromCurrency, origin]);
 
   return (
     <div>
       <h2 className="text-xl my-4">Currency</h2>
       { currencyData ?
         <div>
-          <div>From USD to HKD : {currencyData && currencyFormat(currencyData["usd"]["hkd"])}</div>
+          <div>From {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()} : {currencyData && currencyFormat(currencyData[fromCurrency][toCurrency])}</div>
           <div className="text-sm text-gray-500">as recorded on {currencyData.date}</div>
         </div>
         :
