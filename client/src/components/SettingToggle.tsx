d8d046b535e9731cc4747e31e50228d5c66035d5
Hermanda Mak <hermandamak@gmail.com>
2024-08-15 11:40:16 -0700

    update setting icon toggle display

--- client/src/components/SettingToggle.tsx
+++ client/src/components/SettingToggle.tsx
@@ -1,9 +1,10 @@
-import icon from "../assets/settings-icon.svg"
+import {ReactComponent as Icon} from '../assets/settings-icon.svg'
+
 import { useSetting } from "../hooks/SettingService"
 
 const SettingToggle = () => {
 
-  const { setShowSetting } = useSetting()
+  const { showSetting, setShowSetting } = useSetting()
 
   const displaySettingOptions = () => {
     setShowSetting()
@@ -11,7 +12,7 @@ const SettingToggle = () => {
 
   return (
     <div onClick={displaySettingOptions} className="absolute right-4 top-4">
-      <img src={icon} alt="Setting" className="h-8 w-8"/>
+      <Icon className="h-8 w-8" fill={showSetting ? "#000" : "#999"}/>
     </div>
   )
 }
